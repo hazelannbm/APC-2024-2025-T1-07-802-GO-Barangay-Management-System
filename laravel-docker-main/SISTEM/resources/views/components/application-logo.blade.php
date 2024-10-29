f36784814e6ce1b8b88c60952587d800ee1a4435
Hazel Ann Mones <111345264+hazelannbm@users.noreply.github.com>
2024-10-29 17:39:27 +0000

    updated logo

--- laravel-docker-main/SISTEM/resources/views/components/application-logo.blade.php
+++ laravel-docker-main/SISTEM/resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
 <!-- resources/views/components/application-logo.blade.php -->
-<img src="{{ asset('laravel-docker-main/SISTEM/public/logo/802-GO-LOGO.png') }}" alt="Logo" class="h-16 w-auto">
+<img src="{{ asset('logo/802-GO-LOGO.png') }}" alt="Logo" class="h-16 w-auto">
