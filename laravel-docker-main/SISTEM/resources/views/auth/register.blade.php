3642c532bd37cc6f93c6897f7b1602b918a34b6f
Hazel Ann Mones <111345264+hazelannbm@users.noreply.github.com>
2024-10-30 01:06:35 +0000

    updated register page

--- laravel-docker-main/SISTEM/resources/views/auth/register.blade.php
+++ laravel-docker-main/SISTEM/resources/views/auth/register.blade.php
@@ -113,6 +113,28 @@
                 <x-input-error :messages="$errors->get('valid_id')" class="mt-2" />
             </div>
 
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-input-label for="email" :value="__('Email Address')" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
+
+
             <!-- Register Button -->
             <div class="flex justify-between mt-6">
                 <div class="flex items-center">
