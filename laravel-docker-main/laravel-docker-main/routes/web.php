e298433a2b9de7d358e207e2a1156e89e361f217
Joana Grace Garcia <114065130+14986cm@users.noreply.github.com>
2025-01-14 00:45:43 +0000

    Added Document Request Form and Proper Routing

--- laravel-docker-main/laravel-docker-main/routes/web.php
+++ laravel-docker-main/laravel-docker-main/routes/web.php
@@ -5,6 +5,14 @@ use App\Http\Controllers\DocumentRequestController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 
+Route::get('/document-request', [DocumentRequestController::class, 'index'])->name('document-request');
+
+Route::get('/barangay-clearance', [DocumentRequestController::class, 'barangayClearance'])->name('barangay-clearance');
+Route::get('/certificate-of-residency', [DocumentRequestController::class, 'certificateOfResidency'])->name('certificate-of-residency');
+Route::get('/indigency-certificate', [DocumentRequestController::class, 'indigencyCertificate'])->name('indigency-certificate');
+Route::get('/barangay-id', [DocumentRequestController::class, 'barangayID'])->name('barangay-id');
+Route::get('/business-permit', [DocumentRequestController::class, 'businessPermit'])->name('business-permit');
+
 $url = config('app.url');
 URL::forceRootUrl($url);
 
@@ -39,6 +47,12 @@ Route::get('/document-request', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::post('/submit-barangay-clearance', [DocumentController::class, 'submitBarangayClearance'])->name('submit-barangay-clearance');
+Route::post('/submit-certificate-of-residency', [DocumentController::class, 'submitCertificateOfResidency'])->name('submit-certificate-of-residency');
+Route::post('/submit-indigency-certificate', [DocumentController::class, 'submitIndigencyCertificate'])->name('submit-indigency-certificate');
+Route::post('/submit-barangay-id', [DocumentController::class, 'submitBarangayId'])->name('submit-barangay-id');
+Route::post('/submit-business-permits', [DocumentController::class, 'submitBusinessPermits'])->name('submit-business-permits');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
